5566918b623f2736da7a0848289b60d72672a0e0
Daniel Cocos <daniel.cocos@redbeesoftware.com>
2022-01-09 17:52:07 +0200

    Changed padding

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   pill: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 12,
     paddingVertical: 3,
     backgroundColor: 'white',
     borderRadius: 20,
